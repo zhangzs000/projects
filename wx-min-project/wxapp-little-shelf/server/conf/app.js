5e46932fa7610a3ed79e295531f6fce90ed45434
zhangzs <1461998312@qq.com>
2020-08-08 10:54:31 +0800

    add minpor

---
+++ wx-min-project/wxapp-little-shelf/server/conf/app.js
@@ -0,0 +1,9 @@
+module.exports = {
+    appConfig: {
+        appid: 'wx291f734cef115826',
+        secret: '2f5ebad37bfa13bc1c1e857a232278be'
+    },
+    userConfig: {
+        credit: 30          // 用户初始积分余额
+    }
+}
---
+++ wx-min-project/wxapp-little-shelf/server/mocks/books.js
@@ -0,0 +1,29 @@
+const books = [{
+    bkauthor: '圣哥',
+    bkclass: '1',
+    bkcover: 'http://img3x1.ddimg.cn/48/12/1168614561-1_w_1.jpg',
+    bkfile: 'http://img3x1.ddimg.cn/48/12/1168614561-1_w_1.jpg',
+    bkid: '1',
+    bkname: '<<node开发实战>>',
+    bkprice: 100,
+    bkpublisher:'图灵出版社'
+},{
+    bkauthor: '圣哥',
+    bkclass: '1',
+    bkcover: 'http://img3x1.ddimg.cn/48/12/1168614561-1_w_1.jpg',
+    bkfile: 'http://img3x1.ddimg.cn/48/12/1168614561-1_w_1.jpg',
+    bkid: '1',
+    bkname: '<<node开发实战>>',
+    bkprice: 100,
+    bkpublisher:'图灵出版社'
+},{
+    bkauthor: '圣哥',
+    bkclass: '1',
+    bkcover: 'http://img3x1.ddimg.cn/48/12/1168614561-1_w_1.jpg',
+    bkfile: 'http://img3x1.ddimg.cn/48/12/1168614561-1_w_1.jpg',
+    bkid: '1',
+    bkname: '<<node开发实战>>',
+    bkprice: 100,
+    bkpublisher:'图灵出版社'
+}]
+module.exports =  books
